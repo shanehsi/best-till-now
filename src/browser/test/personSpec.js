a2585f9cae057fffb9605f9e8dae84ce69266b35
Shane Hsi <shane@hsi.rocks>
2016-04-17 16:06:42 +0800

    clean

--- src/browser/test/personSpec.js
+++ src/browser/test/personSpec.js
@@ -1,23 +1,7 @@
 import Person from "../main/Person";
 
-function once(fn) {
-  var returnValue, called = false;
-  return function() {
-    if (!called) {
-      called = true;
-      returnValue = fn.apply(this, arguments);
-    }
-    return returnValue;
-  };
-}
-
 describe("Person", () => {
   it("should report name", () => {
-    const spy = sinon.spy();
-    const proxy = once(spy);
-    proxy();
-
     assert.equal("John", "John");
-    assert(spy.called);
   });
 });
